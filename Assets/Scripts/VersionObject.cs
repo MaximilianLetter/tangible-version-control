84685c8936fa94c41be07b7c3ce73af64f8ac495
MaximilianLetter <maximilian.letter@gmail.com>
2021-04-28 14:11:34 +0200

    Add static information to version objects

---
+++ Assets/Scripts/VersionObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VersionObject : MonoBehaviour
+{
+    public string title;
+    public string description;
+    public string createdAt;
+    public string createdBy;
+}
