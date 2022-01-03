12194e1c9bb7a7f2c775f1bb3cc615b76b65c86c
MaximilianLetter <50317821+MaximilianLetter@users.noreply.github.com>
2022-01-03 13:26:43 +0100

    Read single commit entries including worked on files

--- Assets/Scripts/Managers/GitHubAPIManager.cs
+++ Assets/Scripts/Managers/GitHubAPIManager.cs
@@ -11,7 +11,7 @@ public class GitHubAPIManager : MonoBehaviour
 
     void Start()
     {
-        StartCoroutine(GetListOfBranches());
+        StartCoroutine(GetListOfCommits());
     }
 
     // test get branches
@@ -39,6 +39,56 @@ public class GitHubAPIManager : MonoBehaviour
         for (int i = 0; i < branchesInfo.Count; i++)
         {
             Debug.Log(branchesInfo[i]["name"]);
+        }
+    }
+
+    /// <summary>
+    /// Get a list of all commits of the base repository from GitHub
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator GetListOfCommits()
+    {
+        string commitsURL = baseURL + baseRepo + "commits";
+        // Example URL: https://api.github.com/repos/MaximilianLetter/model-comparison-by-github/commits
+
+        UnityWebRequest repoCommitsRequest = UnityWebRequest.Get(commitsURL);
+
+        yield return repoCommitsRequest.SendWebRequest();
+
+        if (repoCommitsRequest.isNetworkError || repoCommitsRequest.isHttpError)
+        {
+            Debug.LogError(repoCommitsRequest.error);
+            yield break;
+        }
+
+        // Alternative, Unity default approach with serializable objects
+        //JsonUtility.FromJson<TODO_serializable_class>(repoBranchesRequest.downloadHandler.text);
+
+        JSONNode listCommitsInfo = JSON.Parse(repoCommitsRequest.downloadHandler.text);
+
+        Debug.Log("Total amount of commits: " + listCommitsInfo.Count);
+        for (int i = 0; i < listCommitsInfo.Count; i++)
+        {
+            Debug.Log(listCommitsInfo[i]["commit"]["message"]);
+            Debug.Log(listCommitsInfo[i]["sha"]);
+            string commitID = listCommitsInfo[i]["sha"];
+            // what about node id, is that a link to branches?
+
+            string singleCommitURL = baseURL + baseRepo + "commits/" + commitID;
+            UnityWebRequest singleCommitRequest = UnityWebRequest.Get(singleCommitURL);
+
+            yield return singleCommitRequest.SendWebRequest();
+
+            if (singleCommitRequest.isNetworkError || singleCommitRequest.isHttpError)
+            {
+                Debug.LogError(singleCommitRequest.error);
+                yield break;
+            }
+
+            JSONNode singleCommitInfo = JSON.Parse(singleCommitRequest.downloadHandler.text);
+            Debug.Log(singleCommitInfo["files"][0]["filename"]);
+
+            // TODO next: try to load model from file URL of commit
         }
     }
 }
